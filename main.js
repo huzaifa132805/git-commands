4e570812af6fc98e0f201edcc1c4dc7ed737b466
Muhammad Huzaifa <huzaifa132805@gmail.com>
2023-12-23 21:32:16 +0500

    add new files

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+// factorial function
+
+function factorial(x){
+    if (x === 0){
+        return 1
+    }else{
+        return x * factorial(x - 1)
+    }
+}
+
+console.log(factorial(11))
